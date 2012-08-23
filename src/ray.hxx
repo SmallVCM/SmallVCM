46eb23e4a8e1d5a25d29fb9a44019207ab3cacf4
Tomas Davidovic <tomas@davidovic.cz>
2012-08-23 09:16:23 +0200

    Added convenience constructors to Ray and Isect

--- src/ray.hxx
+++ src/ray.hxx
@@ -13,6 +13,10 @@
 // Ray casting
 struct Ray
 {
+    Ray(){}
+    Ray(Vec3f aOrg, Vec3f aDir, float aTMin)
+        : org(aOrg), dir(aDir), tmin(aTMin){}
+
     Vec3f org;  //!< Ray origin
     Vec3f dir;  //!< Ray direction
     float tmin; //!< Minimal distance to intersection
@@ -20,6 +24,8 @@ struct Ray
 
 struct Isect
 {
+    Isect(){}
+    Isect(float aMaxDist):dist(aMaxDist) {}
     float dist;    //!< Distance to closest intersection (serves as ray.tmax)
     int   matID;   //!< ID of intersected material
     int   lightID; //!< ID of intersected light (if < 0, then none)
