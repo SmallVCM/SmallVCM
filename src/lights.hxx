f80347d30049c36d84079adc45842a3607745fa0
Tomas Davidovic <tomas@davidovic.cz>
2012-08-24 21:33:10 +0200

    Removed duplicit comments from lights

--- src/lights.hxx
+++ src/lights.hxx
@@ -73,20 +73,15 @@ public:
         mFrame.SetFromZ(normal);
     }
 
-    /* \brief Illuminates a given point in the scene.
-     *
-     * Given a point and two random samples (e.g., for position on area lights),
-     * this method returns direction from point to light, distance,
-     * pdf of having chosen this direction (e.g., 1 / area).
-     * Optionally also returns pdf of emitting particle in this direction,
-     * and cosine from lights normal (helps with PDF of hitting the light,
-     * but set to 1 for point lights).
-     *
-     * Returns radiance.
-     */
-    virtual Vec3f Illuminate(const Vec3f& aReceivingPosition,
-        const Vec2f& aRndTuple, Vec3f& oDirectionToLight, float& oDistance,
-        float& oDirectPdfW, float* oEmissionPdfW = NULL, float* oCosAtLight = NULL) const
+    virtual Vec3f Illuminate(
+        const SceneSphere &/*aSceneSphere*/,
+        const Vec3f       &aReceivingPosition,
+        const Vec2f       &aRndTuple,
+        Vec3f             &oDirectionToLight,
+        float             &oDistance,
+        float             &oDirectPdfW,
+        float             *oEmissionPdfW = NULL,
+        float             *oCosAtLight = NULL) const
     {
         const Vec2f uv = SampleUniformTriangle(aRndTuple);
         const Vec3f lightPoint = p0 + e1 * uv.x + e2 * uv.y;
@@ -115,20 +110,15 @@ public:
         return mIntensity;
     }
 
-    /* \brief Emits particle from the light.
-     *
-     * Given two sets of random numbers (e.g., position and direction on area light),
-     * this method generates a position and direction for light particle, along
-     * with the pdf.
-     *
-     * Can also supply pdf (w.r.t. area) of choosing this position when calling
-     * Illuminate. Also provides cosine on the light (this is 1 for point lights etc.).
-     *
-     * Returns "energy" that particle carries
-     */
-    virtual Vec3f Emit(const Vec2f &aDirRndTuple, const Vec2f &aPosRndTuple,
-        Vec3f &oPosition, Vec3f &oDirection, float &oEmissionPdfW,
-        float *oDirectPdfA, float *oCosThetaLight) const
+    virtual Vec3f Emit(
+        const SceneSphere &/*aSceneSphere*/,
+        const Vec2f       &aDirRndTuple,
+        const Vec2f       &aPosRndTuple,
+        Vec3f             &oPosition,
+        Vec3f             &oDirection,
+        float             &oEmissionPdfW,
+        float             *oDirectPdfA,
+        float             *oCosThetaLight) const
     {
         const Vec2f uv = SampleUniformTriangle(aPosRndTuple);
         oPosition = p0 + e1 * uv.x + e2 * uv.y;
@@ -149,14 +139,12 @@ public:
         return mIntensity * localOmegaOut.z;
     }
 
-    /* \brief Returns radiance for ray randomly hitting the light
-     *
-     * Given ray direction and hitpoint, it returns radiance.
-     * Can also provide area pdf of sampling hitpoint in Illuminate,
-     * and of emitting particle along the ray (in opposite direction).
-     */
-    virtual Vec3f GetRadiance(const Vec3f &aRayDirection,
-        const Vec3f &aHitPoint, float *oDirectPdfA = NULL, float *oEmissionPdfW = NULL) const
+    virtual Vec3f GetRadiance(
+        const SceneSphere &/*aSceneSphere*/,
+        const Vec3f       &aRayDirection,
+        const Vec3f       &aHitPoint,
+        float             *oDirectPdfA = NULL,
+        float             *oEmissionPdfW = NULL) const
     {
         const float cosOutL = std::max(0.f, Dot(mFrame.Normal(), -aRayDirection));
 
