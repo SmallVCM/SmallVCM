0d553a580897d667d9cad8642cc105d4998a25ba
Tomas Davidovic <tomas@davidovic.cz>
2012-08-30 13:44:31 +0200

    Changes to HTML formatting, and table width set to fixed value

--- src/html_writer.hxx
+++ src/html_writer.hxx
@@ -74,95 +74,95 @@ public:
         mHtml << "<script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js\"></script>" << std::endl;
         mHtml << "<script type=\"text/javascript\">" << std::endl;
         mHtml << "<!--" << std::endl;
-        mHtml << "	(function($){" << std::endl;
+        mHtml << "    (function($){" << std::endl;
         mHtml << "    $.fn.extend({" << std::endl;
         mHtml << "        //plugin name - qbeforeafter" << std::endl;
         mHtml << "        qbeforeafter: function(options) {" << std::endl;
         mHtml << "" << std::endl;
         mHtml << "            return this.each(function() {" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "	            var i = $(this);" << std::endl;
-        mHtml << "				var img_layer_tl = i.children('img:eq(0)').attr('src');" << std::endl;
-        mHtml << "				var img_layer_tr = i.children('img:eq(1)').attr('src');" << std::endl;
-        mHtml << "				var img_layer_bl = i.children('img:eq(2)').attr('src');" << std::endl;
-        mHtml << "				var img_layer_br = i.children('img:eq(3)').attr('src');" << std::endl;
-        mHtml << "				var img_cap_tl = i.children('img:eq(0)').attr('alt');" << std::endl;
-        mHtml << "				var img_cap_tr = i.children('img:eq(1)').attr('alt');" << std::endl;
-        mHtml << "				var img_cap_bl = i.children('img:eq(2)').attr('alt');" << std::endl;
-        mHtml << "				var img_cap_br = i.children('img:eq(3)').attr('alt');" << std::endl;
+        mHtml << "                var i = $(this);" << std::endl;
+        mHtml << "                var img_layer_tl = i.children('img:eq(0)').attr('src');" << std::endl;
+        mHtml << "                var img_layer_tr = i.children('img:eq(1)').attr('src');" << std::endl;
+        mHtml << "                var img_layer_bl = i.children('img:eq(2)').attr('src');" << std::endl;
+        mHtml << "                var img_layer_br = i.children('img:eq(3)').attr('src');" << std::endl;
+        mHtml << "                var img_cap_tl = i.children('img:eq(0)').attr('alt');" << std::endl;
+        mHtml << "                var img_cap_tr = i.children('img:eq(1)').attr('alt');" << std::endl;
+        mHtml << "                var img_cap_bl = i.children('img:eq(2)').attr('alt');" << std::endl;
+        mHtml << "                var img_cap_br = i.children('img:eq(3)').attr('alt');" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				var width = i.children('img:eq(0)').width();" << std::endl;
-        mHtml << "				var height = i.children('img:eq(0)').height();" << std::endl;
+        mHtml << "                var width = i.children('img:eq(0)').width();" << std::endl;
+        mHtml << "                var height = i.children('img:eq(0)').height();" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.children('img').hide();" << std::endl;
+        mHtml << "                i.children('img').hide();" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.css({'overflow': 'hidden', 'position': 'relative'});" << std::endl;
-        mHtml << "				i.append('<div class=\"ba-layer_tl\"></div>');" << std::endl;
-        mHtml << "				i.append('<div class=\"ba-layer_tr\"></div>');" << std::endl;
-        mHtml << "				i.append('<div class=\"ba-layer_bl\"></div>');" << std::endl;
-        mHtml << "				i.append('<div class=\"ba-layer_br\"></div>');" << std::endl;
+        mHtml << "                i.css({'overflow': 'hidden', 'position': 'relative'});" << std::endl;
+        mHtml << "                i.append('<div class=\"ba-layer_tl\"></div>');" << std::endl;
+        mHtml << "                i.append('<div class=\"ba-layer_tr\"></div>');" << std::endl;
+        mHtml << "                i.append('<div class=\"ba-layer_bl\"></div>');" << std::endl;
+        mHtml << "                i.append('<div class=\"ba-layer_br\"></div>');" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.append('<div class=\"ba-caption_tl\">' + img_cap_tl + '</div>');" << std::endl;
-        mHtml << "				i.append('<div class=\"ba-caption_tr\">' + img_cap_tr + '</div>');" << std::endl;
-        mHtml << "				i.append('<div class=\"ba-caption_bl\">' + img_cap_bl + '</div>');" << std::endl;
-        mHtml << "				i.append('<div class=\"ba-caption_br\">' + img_cap_br + '</div>');" << std::endl;
+        mHtml << "                i.append('<div class=\"ba-caption_tl\">' + img_cap_tl + '</div>');" << std::endl;
+        mHtml << "                i.append('<div class=\"ba-caption_tr\">' + img_cap_tr + '</div>');" << std::endl;
+        mHtml << "                i.append('<div class=\"ba-caption_bl\">' + img_cap_bl + '</div>');" << std::endl;
+        mHtml << "                i.append('<div class=\"ba-caption_br\">' + img_cap_br + '</div>');" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.children('.ba-layer_tl, .ba-layer_tr, .ba-layer_bl, .ba-layer_br').width(width);" << std::endl;
-        mHtml << "				i.children('.ba-layer_tl, .ba-layer_tr, .ba-layer_bl, .ba-layer_br').height(height);" << std::endl;
+        mHtml << "                i.children('.ba-layer_tl, .ba-layer_tr, .ba-layer_bl, .ba-layer_br').width(width);" << std::endl;
+        mHtml << "                i.children('.ba-layer_tl, .ba-layer_tr, .ba-layer_bl, .ba-layer_br').height(height);" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.children('.ba-layer_tl').css('backgroundImage','url(' + img_layer_tl + ')');" << std::endl;
-        mHtml << "				i.children('.ba-layer_tr').css('backgroundImage','url(' + img_layer_tr + ')');" << std::endl;
-        mHtml << "				i.children('.ba-layer_bl').css('backgroundImage','url(' + img_layer_bl + ')');" << std::endl;
-        mHtml << "				i.children('.ba-layer_br').css('backgroundImage','url(' + img_layer_br + ')');" << std::endl;
+        mHtml << "                i.children('.ba-layer_tl').css('backgroundImage','url(' + img_layer_tl + ')');" << std::endl;
+        mHtml << "                i.children('.ba-layer_tr').css('backgroundImage','url(' + img_layer_tr + ')');" << std::endl;
+        mHtml << "                i.children('.ba-layer_bl').css('backgroundImage','url(' + img_layer_bl + ')');" << std::endl;
+        mHtml << "                i.children('.ba-layer_br').css('backgroundImage','url(' + img_layer_br + ')');" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.children('.ba-layer_tl').width(width * 0.5);" << std::endl;
-        mHtml << "				i.children('.ba-layer_tl').height(height * 0.5);" << std::endl;
-        mHtml << "				i.children('.ba-layer_tr').height(height * 0.5);" << std::endl;
-        mHtml << "				i.children('.ba-layer_bl').width(width * 0.5);" << std::endl;
+        mHtml << "                i.children('.ba-layer_tl').width(width * 0.5);" << std::endl;
+        mHtml << "                i.children('.ba-layer_tl').height(height * 0.5);" << std::endl;
+        mHtml << "                i.children('.ba-layer_tr').height(height * 0.5);" << std::endl;
+        mHtml << "                i.children('.ba-layer_bl').width(width * 0.5);" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.children('.ba-caption_tl').show();" << std::endl;
-        mHtml << "				i.children('.ba-caption_tr').show();" << std::endl;
-        mHtml << "				i.children('.ba-caption_bl').show();" << std::endl;
-        mHtml << "				i.children('.ba-caption_br').show();" << std::endl;
+        mHtml << "                i.children('.ba-caption_tl').show();" << std::endl;
+        mHtml << "                i.children('.ba-caption_tr').show();" << std::endl;
+        mHtml << "                i.children('.ba-caption_bl').show();" << std::endl;
+        mHtml << "                i.children('.ba-caption_br').show();" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.children('.ba-caption_tl').css({ bottom: height * 0.5, right: width * 0.5 });" << std::endl;
-        mHtml << "				i.children('.ba-caption_tr').css({ bottom: height * 0.5, left:  width * 0.5 });" << std::endl;
-        mHtml << "				i.children('.ba-caption_bl').css({ top:    height * 0.5, right: width * 0.5 });" << std::endl;
-        mHtml << "				i.children('.ba-caption_br').css({ top:    height * 0.5, left:  width * 0.5 });" << std::endl;
+        mHtml << "                i.children('.ba-caption_tl').css({ bottom: height * 0.5, right: width * 0.5 });" << std::endl;
+        mHtml << "                i.children('.ba-caption_tr').css({ bottom: height * 0.5, left:  width * 0.5 });" << std::endl;
+        mHtml << "                i.children('.ba-caption_bl').css({ top:    height * 0.5, right: width * 0.5 });" << std::endl;
+        mHtml << "                i.children('.ba-caption_br').css({ top:    height * 0.5, left:  width * 0.5 });" << std::endl;
         mHtml << "" << std::endl;
         mHtml << "            }).mousemove(function (e) {" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				var o = options;" << std::endl;
-        mHtml << "				var i = $(this);" << std::endl;
+        mHtml << "                var o = options;" << std::endl;
+        mHtml << "                var i = $(this);" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				right_border_width = parseInt(i.children('.ba-layer_tl').css(\"borderRightWidth\"), 10);" << std::endl;
-        mHtml << "				bottom_border_width = parseInt(i.children('.ba-layer_tl').css(\"borderBottomWidth\"), 10);" << std::endl;
-        mHtml << "				pos_imgX = i.position()['left'];" << std::endl;
-        mHtml << "				pos_imgY = i.position()['top'];" << std::endl;
-        mHtml << "				pos_mouseX = e.pageX - right_border_width * 0.5;" << std::endl;
-        mHtml << "				pos_mouseY = e.pageY - bottom_border_width * 0.5;" << std::endl;
-        mHtml << "				new_width  = pos_mouseX - pos_imgX;" << std::endl;
-        mHtml << "				new_height = pos_mouseY - pos_imgY;" << std::endl;
-        mHtml << "				img_width  = i.width();" << std::endl;
-        mHtml << "				img_height = i.height();" << std::endl;
-        mHtml << "				img_cap_tl = i.children('img:eq(0)').attr('alt');" << std::endl;
-        mHtml << "				img_cap_tr = i.children('img:eq(1)').attr('alt');" << std::endl;
-        mHtml << "				img_cap_bl = i.children('img:eq(2)').attr('alt');" << std::endl;
-        mHtml << "				img_cap_br = i.children('img:eq(3)').attr('alt');" << std::endl;
+        mHtml << "                right_border_width = parseInt(i.children('.ba-layer_tl').css(\"borderRightWidth\"), 10);" << std::endl;
+        mHtml << "                bottom_border_width = parseInt(i.children('.ba-layer_tl').css(\"borderBottomWidth\"), 10);" << std::endl;
+        mHtml << "                pos_imgX = i.position()['left'];" << std::endl;
+        mHtml << "                pos_imgY = i.position()['top'];" << std::endl;
+        mHtml << "                pos_mouseX = e.pageX - right_border_width * 0.5;" << std::endl;
+        mHtml << "                pos_mouseY = e.pageY - bottom_border_width * 0.5;" << std::endl;
+        mHtml << "                new_width  = pos_mouseX - pos_imgX;" << std::endl;
+        mHtml << "                new_height = pos_mouseY - pos_imgY;" << std::endl;
+        mHtml << "                img_width  = i.width();" << std::endl;
+        mHtml << "                img_height = i.height();" << std::endl;
+        mHtml << "                img_cap_tl = i.children('img:eq(0)').attr('alt');" << std::endl;
+        mHtml << "                img_cap_tr = i.children('img:eq(1)').attr('alt');" << std::endl;
+        mHtml << "                img_cap_bl = i.children('img:eq(2)').attr('alt');" << std::endl;
+        mHtml << "                img_cap_br = i.children('img:eq(3)').attr('alt');" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.children('.ba-layer_tl').width(new_width);" << std::endl;
-        mHtml << "				i.children('.ba-layer_tl').height(new_height);" << std::endl;
-        mHtml << "				i.children('.ba-layer_tr').height(new_height);" << std::endl;
-        mHtml << "				i.children('.ba-layer_bl').width(new_width);" << std::endl;
+        mHtml << "                i.children('.ba-layer_tl').width(new_width);" << std::endl;
+        mHtml << "                i.children('.ba-layer_tl').height(new_height);" << std::endl;
+        mHtml << "                i.children('.ba-layer_tr').height(new_height);" << std::endl;
+        mHtml << "                i.children('.ba-layer_bl').width(new_width);" << std::endl;
         mHtml << "" << std::endl;
-        mHtml << "				i.children('.ba-caption_tl').css({ bottom: img_height - new_height, right: img_width - new_width });" << std::endl;
-        mHtml << "				i.children('.ba-caption_tr').css({ bottom: img_height - new_height, left:  new_width });" << std::endl;
-        mHtml << "				i.children('.ba-caption_bl').css({ top:    new_height,              right: img_width - new_width });" << std::endl;
-        mHtml << "				i.children('.ba-caption_br').css({ top:    new_height,              left:  new_width });" << std::endl;
-        mHtml << "			});" << std::endl;
+        mHtml << "                i.children('.ba-caption_tl').css({ bottom: img_height - new_height, right: img_width - new_width });" << std::endl;
+        mHtml << "                i.children('.ba-caption_tr').css({ bottom: img_height - new_height, left:  new_width });" << std::endl;
+        mHtml << "                i.children('.ba-caption_bl').css({ top:    new_height,              right: img_width - new_width });" << std::endl;
+        mHtml << "                i.children('.ba-caption_br').css({ top:    new_height,              left:  new_width });" << std::endl;
+        mHtml << "            });" << std::endl;
         mHtml << "        }" << std::endl;
         mHtml << "    });" << std::endl;
-        mHtml << "	})(jQuery);" << std::endl;
+        mHtml << "    })(jQuery);" << std::endl;
         mHtml << "-->" << std::endl;
         mHtml << "</script>" << std::endl;
         mHtml << "" << std::endl;
@@ -188,8 +188,11 @@ public:
 
     void AddScene(const std::string &aSceneName)
     {
-        mHtml << "<table><tr><td colspan=\""
-            << mAlgorithmCount << "\"><h2>" << aSceneName << "</h2></td></tr>" << std::endl;
+
+        mHtml << "<table";
+        if(mAlgorithmCount < 100)
+            mHtml << " width=\"" << mAlgorithmCount * (mThumbnailSize + 10) << "px\"";
+        mHtml << "><tr><td colspan=\"" << mAlgorithmCount << "\"><h2>" << aSceneName << "</h2></td></tr>" << std::endl;
         mHtml << "<tr>" << std::endl;
     }
 
@@ -211,7 +214,7 @@ public:
         else if(aBorderColor == kRed)
             mHtml << "style=\"border:5px solid #f00\" ";
         else
-            mHtml << "style=\"border:5px solid #fff\" ";
+            mHtml << "style=\"border:5px solid #ccc\" ";
         mHtml << " alt=\"" << aFileName << " (" << aTime << " s)\" ";
         mHtml << "height=\""<< mThumbnailSize <<"px\" />";
 #else
@@ -238,7 +241,7 @@ public:
         mHtml << "</a>" << std::endl;
 
         // The text
-        mHtml << "<small>" << aMethodName
+        mHtml << "<br/><small>" << aMethodName
             << " (" << aTime << " s)" << aOtherInfo
             << "</small></td>" << std::endl;
     }
